998546d77e849f94dc330dd943d0b746525e2303
João Pedro Figueiredo <joaopedrofvalle@gmail.com>
2025-09-01 20:19:08 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,17 @@
 <?php
 session_start();
-$isAdmin = (isset($_SESSION['tipo']) && $_SESSION['tipo'] === 'terapeuta');
+$isAdmin = isset($_SESSION['usuario_id']) && ($_SESSION['tipo'] ?? '') === 'terapeuta';
+$isUser  = isset($_SESSION['usuario_id']) && !$isAdmin;
 ?>
 <div class="header-admin">
-  <div><?= $isAdmin ? "Painel Adm." : "Perfil" ?></div>
-  <div class="menu-horizontal">
-    <!-- coloque seus botões aqui -->
-  </div>
+    <div id="li-perfil-login">
+        <?php if ($isAdmin): ?>
+            <a href="/admin/index.php">Painel Adm.</a>
+        <?php elseif ($isUser): ?>
+            <a href="/perfil.html">Perfil</a>
+        <?php else: ?>
+            <a href="/registrar.html">Login</a>
+        <?php endif; ?>
+    </div>
+    <div class="menu-horizontal"><!-- botões --></div>
 </div>
