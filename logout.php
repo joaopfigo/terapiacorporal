1358ec06468d4470208af97d4824de11c8ab9c95
João Pedro Figueiredo <joaopedrofvalle@gmail.com>
2025-09-01 21:47:16 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
-header('Location: /registrar.html'); // ou outro arquivo público de login
+header('Location: registrar.html'); // ou outro arquivo público de login
 exit;
 ?>
