8d29fb566f94031a6f0c5bed955210f5e092ce97
João Pedro Figueiredo <joaopedrofvalle@gmail.com>
2025-10-01 03:34:49 -0300

    Create booking_constants.php

---
+++ lib/booking_constants.php
@@ -0,0 +1,8 @@
+<?php
+// Constantes compartilhadas referentes ao combo de dois tratamentos
+if (!defined('DUO_SERVICE_ID')) {
+    define('DUO_SERVICE_ID', 10);
+}
+if (!defined('DUO_PRECO')) {
+    define('DUO_PRECO', 260.00);
+}
