388edf44914bcb07cc648aa298edbbd311e68119
João Pedro Figueiredo <joaopedrofvalle@gmail.com>
2025-10-09 16:10:00 -0300

    Update paciente.php

--- admin/paciente.php
+++ admin/paciente.php
@@ -63,6 +63,7 @@ if ($row = $res->fetch_assoc()) {
 <html lang='pt-br'>
 <head>
 <meta charset='UTF-8'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Paciente: <?= htmlspecialchars($pac['nome']) ?></title>
 <link rel="stylesheet" href="css/paciente.css">
 <style>
